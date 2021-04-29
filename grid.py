57492d561bd72d0d6b8ec9afdf54a43b5cecea1b
Carlo Keizer <ck.soest@hotmail.com>
2021-04-29 17:06:58 +0200

    td learning

--- grid.py
+++ grid.py
@@ -3,21 +3,18 @@ import copy
 from itertools import product
 import random
 import matplotlib.pyplot as plt
-from collections import defaultdict
 from typing import Tuple
 
 
 class Grid:
-    def __init__(self, rewards, terminal_states, gamma, exploring_starts):
+    def __init__(self, rewards, policy, terminal_states, gamma):
         self.rewards = rewards
+        self.policy = policy
         self.terminal_states = terminal_states
+        self.start_points = self.generate_start_points()
         self.gamma = gamma
         self.values = np.zeros(self.rewards.shape)
-        self.greedy_policy = np.zeros(self.rewards.shape)
-        self.greedy_policy = self.greedy_policy.tolist()
-        self.start_points = self.generate_start_points()
         self.returns = {(i, j): [] for i, j in product(range(rewards.shape[0]), range(rewards.shape[1]))}
-        self.exploring_starts = exploring_starts
 
     def generate_start_points(self):
         start_points = []
@@ -28,86 +25,98 @@ class Grid:
                 start_points.append((i, j))
         return start_points
 
+    @staticmethod
+    def new_state_based_on_action(current_state, action):
+        u = (current_state[0] - 1, current_state[1])
+        r = (current_state[0], current_state[1] + 1)
+        d = (current_state[0] + 1, current_state[1])
+        l = (current_state[0], current_state[1] - 1)
+
+        if action == "U":
+            chance = random.random()
+            if chance > 0.3:
+                new_state = u
+            elif chance < 0.3 and chance > 0.2:
+                new_state = r
+            elif chance < 0.2 and chance > 0.1:
+                new_state = d
+            else:
+                new_state = r
+
+        elif action == "R":
+            chance = random.random()
+            if chance > 0.3:
+                new_state = r
+            elif chance < 0.3 and chance > 0.2:
+                new_state = d
+            elif chance < 0.2 and chance > 0.1:
+                new_state = l
+            else:
+                new_state = u
+
+        elif action == "D":
+            chance = random.random()
+            if chance > 0.3:
+                new_state = d
+            elif chance < 0.3 and chance > 0.2:
+                new_state = l
+            elif chance < 0.2 and chance > 0.1:
+                new_state = u
+            else:
+                new_state = r
+
+        else:
+            chance = random.random()
+            if chance > 0.3:
+                new_state = l
+            elif chance < 0.3 and chance > 0.2:
+                new_state = u
+            elif chance < 0.2 and chance > 0.1:
+                new_state = r
+            else:
+                new_state = d
+        return new_state
+
     def generate_episode(self):
         episode = []
-        actions = ["U", "R", "D", "L"]
-
-        if self.exploring_starts:
-            current_state = start_state
-        else:
-            current_state = random.choice(self.start_points)
-
+        current_state = random.choice(self.start_points)
         episode.append(current_state)
+
         while current_state not in self.terminal_states:
-            action = random.choice(actions)
-            u = (current_state[0] - 1, current_state[1])
-            r = (current_state[0], current_state[1] + 1)
-            d = (current_state[0] + 1, current_state[1])
-            l = (current_state[0], current_state[1] - 1)
+            action = self.policy[current_state[0]][current_state[1]]
+            new_state = self.new_state_based_on_action(current_state, action)
 
-            if action == "U":
-                chance = random.random()
-                if chance > 0.3:
-                    new_state = u
-                elif chance < 0.3 and chance > 0.2:
-                    new_state = r
-                elif chance < 0.2 and chance > 0.1:
-                    new_state = d
-                else:
-                    new_state = r
-
-            elif action == "R":
-                chance = random.random()
-                if chance > 0.3:
-                    new_state = r
-                elif chance < 0.3 and chance > 0.2:
-                    new_state = d
-                elif chance < 0.2 and chance > 0.1:
-                    new_state = l
-                else:
-                    new_state = u
-
-            elif action == "D":
-                chance = random.random()
-                if chance > 0.3:
-                    new_state = d
-                elif chance < 0.3 and chance > 0.2:
-                    new_state = l
-                elif chance < 0.2 and chance > 0.1:
-                    new_state = u
-                else:
-                    new_state = r
-
-            else:
-                chance = random.random()
-                if chance > 0.3:
-                    new_state = l
-                elif chance < 0.3 and chance > 0.2:
-                    new_state = u
-                elif chance < 0.2 and chance > 0.1:
-                    new_state = r
-                else:
-                    new_state = d
-
-            if new_state[0] in np.arange(self.rewards.shape[0]) and new_state[1] in np.arange(
-                    self.rewards.shape[1]):
+            if new_state[0] in np.arange(self.rewards.shape[0]) and new_state[1] in np.arange(self.rewards.shape[1]):
                 current_state = new_state
 
             episode[-1] = (episode[-1], action, self.rewards[current_state[0]][current_state[1]])
-            if current_state not in self.terminal_states:
-                episode.append(current_state)
+            episode.append(current_state)
         return episode
 
     def monte_carlo_policy_evaluation(self):
         episode = self.generate_episode()
         episode_copy = copy.deepcopy(episode)
         g = 0
-        for step in reversed(episode):
+        for step in reversed(episode[:-1]):
             episode_copy.remove(step)
             g = self.gamma*g + step[2]
-            if step[0] not in [step[0] for step in episode_copy]:
+            if step[0] not in [step_2[0] for step_2 in episode_copy]:
                 self.returns[step[0]].append(g)
                 self.values[step[0][0]][step[0][1]] = np.mean(self.returns[step[0]])
+
+    def temporal_difference_learning(self, step_size):
+        current_state = random.choice(self.start_points)
+        while current_state not in self.terminal_states:
+            action = self.policy[current_state[0]][current_state[1]]
+            new_state = self.new_state_based_on_action(current_state, action)
+            if new_state[0] in np.arange(self.rewards.shape[0]) and new_state[1] in np.arange(self.rewards.shape[1]):
+                reward = self.rewards[new_state[0]][new_state[1]]
+                self.values[current_state[0]][current_state[1]] += step_size * (reward + self.gamma * self.values[new_state[0]][new_state[1]] - self.values[current_state[0]][current_state[1]])
+                current_state = new_state
+
+            else:
+                reward = self.rewards[current_state[0]][current_state[1]]
+                self.values[current_state[0]][current_state[1]] += step_size * (reward + self.gamma * self.values[current_state[0]][current_state[1]] - self.values[current_state[0]][current_state[1]])
 
     def print_values(self):
         # z = self.values
@@ -124,10 +133,16 @@ class Grid:
             print(values)
         print("\n")
 
-    def run(self, amount_iterations, verbose=False):
-
+    def run_mc(self, amount_iterations, verbose=False):
         for i in range(amount_iterations):
             print("iteration {}".format(i))
             self.monte_carlo_policy_evaluation()
+            if verbose:
+                self.print_values()
+
+    def run_td(self, amount_iterations, step_size, verbose=False):
+        for i in range(amount_iterations):
+            print("iteration {}".format(i))
+            self.temporal_difference_learning(step_size)
             if verbose:
                 self.print_values()
--- main.py
+++ main.py
@@ -8,9 +8,18 @@ rewards = np.array([
             [-1, -1, -1, -1],
             [10, -2, -1, -1]
         ])
+policy = [
+    ["D", "L", "R", "X"],
+    ["D", "R", "D", "U"],
+    ["R", "U", "D", "U"],
+    ["X", "L", "R", "U"]
+]
 terminal_states = [(0, 3), (3, 0)]
 gamma = 1
 
-grid = Grid(rewards, terminal_states, gamma, False)
-grid.monte_carlo_policy_evaluation()
-grid.run(500, verbose=True)
+grid = Grid(rewards, policy, terminal_states, gamma)
+# grid.generate_episode()
+# grid.monte_carlo_policy_evaluation()
+# grid.temporal_difference_learning(0.01)
+# grid.run_td(500, 0.01, verbose=True)
+grid.run_mc(500, verbose=True)
