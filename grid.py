e84b23a251c0af69ded761a50bae4c7666cacf2e
Carlo Keizer <ck.soest@hotmail.com>
2021-05-06 13:41:18 +0200

    q-learning

--- grid.py
+++ grid.py
@@ -3,13 +3,13 @@ import copy
 from itertools import product
 import random
 import matplotlib.pyplot as plt
-from typing import Tuple
 
 
 class Grid:
     def __init__(self, rewards, policy, terminal_states, gamma):
         self.rewards = rewards
         self.policy = policy
+        self.soft_policy = {(i, j, a): 0 for i, j, a in product(range(rewards.shape[0]), range(rewards.shape[1]), ["U", "R", "D", "L"])}
         self.terminal_states = terminal_states
         self.start_points = self.generate_start_points()
         self.gamma = gamma
@@ -78,14 +78,24 @@ class Grid:
                 new_state = d
         return new_state
 
-    def generate_episode(self):
+    def generate_episode(self, soft=False):
         episode = []
         current_state = random.choice(self.start_points)
         episode.append(current_state)
 
         while current_state not in self.terminal_states:
-            action = self.policy[current_state[0]][current_state[1]]
-            new_state = self.new_state_based_on_action(current_state, action)
+            if soft:
+                u = self.soft_policy[(current_state[0], current_state[1], "U")]
+                r = self.soft_policy[(current_state[0], current_state[1], "R")]
+                d = self.soft_policy[(current_state[0], current_state[1], "D")]
+                l = self.soft_policy[(current_state[0], current_state[1], "L")]
+                actions = ["U", "R", "D", "L"]
+                action_weights = [u, r, d, l]
+                action = random.choices(actions, weights=action_weights, k=1)[0]
+                new_state = self.new_state_based_on_action(current_state, action)
+            else:
+                action = self.policy[current_state[0]][current_state[1]]
+                new_state = self.new_state_based_on_action(current_state, action)
 
             if new_state[0] in np.arange(self.rewards.shape[0]) and new_state[1] in np.arange(self.rewards.shape[1]):
                 current_state = new_state
@@ -119,8 +129,8 @@ class Grid:
                 reward = self.rewards[current_state[0]][current_state[1]]
                 self.values[current_state[0]][current_state[1]] += step_size * (reward + self.gamma * self.values[current_state[0]][current_state[1]] - self.values[current_state[0]][current_state[1]])
 
-    def sarsa(self, step_size):
-        current_state = (0, 0)
+    def sarsa(self, step_size, max_q=False):
+        current_state = random.choice(self.start_points)
         qs = {}
         for k in self.q:
             if k[0] == current_state[0] and k[1] == current_state[1]:
@@ -148,11 +158,36 @@ class Grid:
                 reward = self.rewards[current_state[0]][current_state[1]]
                 self.q[(current_state[0], current_state[1], action)] += step_size * (reward+self.gamma*self.q[(current_state[0], current_state[1], action)] - self.q[(current_state[0], current_state[1], action)])
 
+    def q_learning(self, step_size):
+        current_state = random.choice(self.start_points)
+        while current_state not in self.terminal_states:
+            qs = {}
+            for k in self.q:
+                if k[0] == current_state[0] and k[1] == current_state[1]:
+                    qs[k] = self.q[k]
+            max_q = max(qs, key=qs.get)
+            action = max_q[2]
+            max_q = qs[max_q]
 
-    def print_q(self):
-        pass
+            new_state = self.new_state_based_on_action(current_state, action)
+            if new_state[0] in np.arange(self.rewards.shape[0]) and new_state[1] in np.arange(self.rewards.shape[1]):
+                qs_1 = {}
+                for k in self.q:
+                    if k[0] == new_state[0] and k[1] == new_state[1]:
+                        qs_1[k] = self.q[k]
+                max_q1 = max(qs_1, key=qs_1.get)
+                action = max_q1[2]
+                max_q1 = qs_1[max_q1]
+
+                reward = self.rewards[new_state[0]][new_state[1]]
+                self.q[(current_state[0], current_state[1], action)] += step_size * (reward + self.gamma * max_q1 - self.q[(current_state[0], current_state[1], action)])
+                current_state = new_state
+            else:
+                reward = self.rewards[current_state[0]][current_state[1]]
+                self.q[(current_state[0], current_state[1], action)] += step_size * (reward + self.gamma * max_q - self.q[(current_state[0], current_state[1], action)])
 
 
+    # def on_policy_first_visit_monte_carlo_control(self, epsilon):
 
 
     def print_values(self):
@@ -188,5 +223,12 @@ class Grid:
         for i in range(amount_iterations):
             print("iteration {}".format(i))
             self.sarsa(step_size)
+            if verbose:
+                self.print_values()
+
+    def run_q_learning(self, amount_iterations, step_size, verbose=False):
+        for i in range(amount_iterations):
+            print("iteration {}".format(i))
+            self.q_learning(step_size)
             if verbose:
                 self.print_values()
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-# a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# a.reverse()
-# for i in a:
-#     print(i)
-#     a.remove((i))
-#     print(a)
-#
-# print([1,2,3,4,5,6,7,8,9][0:-1])
 import random
-print(random.random())
+
+sampleList = [100, 200, 300, 400, 500]
+
+randomList = random.choices(sampleList, weights=[10, 20, 30, 40, 50], k=1)[0]
+
+print(randomList)
+
